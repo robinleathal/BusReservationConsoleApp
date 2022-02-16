db4871d94de7b5390ad5238fc714e5fa967a6921
Rabin Awal <robinleathal@gmail.com>
2022-02-16 03:32:15 -0600

    Fixed typos and added implementation for Customer menu

--- src/com/brs/services/UserDetails.java
+++ src/com/brs/services/UserDetails.java
@@ -17,7 +17,7 @@ public class UserDetails {
 			System.out.println("Admin Menu");
 			System.out.println("1. Add a Bus");
 			System.out.println("2. View All Buses");
-			System.out.println("3. View A Bs");
+			System.out.println("3. View A Bus");
 			System.out.println("4. Update a Bus");
 			System.out.println("5. Delete a Bus");
 			System.out.println("6. Back");
@@ -51,7 +51,7 @@ public class UserDetails {
 				System.out.println("*****************************************");
 				if (b != null) {
 					System.out.println(b.getId()+"\tt" + b.getType()+"\tt"+ b.getRouteFrom()+"\tt"+ b.getRouteTo()+"\tt"+ b.getTotalSeats());
-					//System.out.println(pDaoImpl.viewProduct(id));
+					
 				}
 				break;
 			case 4:
@@ -80,9 +80,51 @@ public class UserDetails {
 			System.out.println("1. View All Buses");
 			System.out.println("2. View A Bus");
 			System.out.println("3. Book a Ticket");
-			System.out.println("3. Cancel a Ticket");
-			System.out.println("4. Back");
+			System.out.println("4. Cancel a Ticket");
+			System.out.println("5. Back");
+			System.out.println("Choose an Option");
+			int choice = sc.nextInt();
+			switch (choice) {
 			
+			case 1:
+				
+				List<Bus> busList = busDaoImplObj.viewAllBuses();
+				System.out.println("*****************************************");
+				System.out.println("Bus ID \t  Type\t From \t To \t Price \t Seats");
+				System.out.println("*****************************************");
+				for (Bus b: busList) {
+					System.out.println(b.getId()+"\tt" + b.getType()+"\tt"+ b.getRouteFrom()+"\tt"+ b.getRouteTo()+"\tt"+ b.getTotalSeats());
+					
+				}
+				
+				
+				busDaoImplObj.viewAllBuses();
+				break;
+			case 2:
+				System.out.println("Enter Bus Id");
+				
+				int id= sc.nextInt();
+				Bus b = busDaoImplObj.viewBus(id);
+				System.out.println("*****************************************");
+				System.out.println("Bus ID \t  Type\t From \t To \t Price \t Seats");
+				System.out.println("*****************************************");
+				if (b != null) {
+					System.out.println(b.getId()+"\tt" + b.getType()+"\tt"+ b.getRouteFrom()+"\tt"+ b.getRouteTo()+"\tt"+ b.getTotalSeats());
+					
+				}
+				break;
+			case 3:
+				//book a ticket
+				break;
+			case 4:
+				//cancel code
+				break;
+			case 5:
+				ReservationClient.main(null);
+				break;
+			default:
+				System.out.println("Choose between 1 to 5");
+			}
 		}
 	}
 }
